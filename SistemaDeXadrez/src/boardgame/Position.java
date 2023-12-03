1cf1c2aaffd1d9a8824f78bb5af513965566b16a
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-03 19:46:34 -0300

    First class: Position

---
+++ SistemaDeXadrez/src/boardgame/Position.java
@@ -0,0 +1,41 @@
+package boardgame;
+
+public class Position {
+	/*
+	 * attributes section
+	 */
+	private int row;
+	private int column;
+
+	/*
+	 * constructors section
+	 */
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	/*
+	 * getters and setters section
+	 */
+	public int getRow() {
+		return this.row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return this.column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%d, %d", this.getRow(), this.getColumn());
+	}
+}
