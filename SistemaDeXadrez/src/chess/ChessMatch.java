55c93849ccee146ee02fc7f72571be77c5e203d6
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-03 22:30:02 -0300

    Little improvement in board printing

--- SistemaDeXadrez/src/chess/ChessMatch.java
+++ SistemaDeXadrez/src/chess/ChessMatch.java
@@ -9,7 +9,7 @@ public class ChessMatch {
 	 * attributes section
 	 */
 	private Board board;
-	
+
 	/*
 	 * constructors section
 	 */
@@ -17,7 +17,7 @@ public class ChessMatch {
 		this.board = new Board(8, 8);
 		this.initialSetup();
 	}
-	
+
 	/*
 	 * methods section
 	 */
@@ -30,14 +30,24 @@ public class ChessMatch {
 		}
 		return matriz;
 	}
-	
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		this.board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
+
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(this.board, Color.WHITE));
-		placeNewPiece('e', 8, new King(this.board, Color.BLACK));
-		placeNewPiece('e', 1, new King(this.board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
